a2d449b9ba242fbaba51737c8125fb19780137a8
Rachel1214 <104809560+RuixueSun1214@users.noreply.github.com>
2022-05-29 18:45:16 +1000

    Add files via upload

---
+++ PyBank/main.py
@@ -0,0 +1,67 @@
+import os
+
+import csv
+import statistics
+
+budget_data_csv = os.path.join('/Users/rorosun/Desktop/python-challenge/PyBank/Resources', 'budget_data.csv')
+
+
+monthCount = 0
+totalVolume = 0
+greatestIncrease = 0
+bestMonth = ''
+greatestDecrease = 0
+worstMonth = ''
+
+
+
+change = []
+monthToMonthChange = []
+
+with open(budget_data_csv, newline='') as csvfile:
+
+    csvreader = csv.reader(csvfile, delimiter=',')
+
+    # Read the header row first 
+    csv_header = next(csvreader)
+
+    # Read each row of data after the header
+    for row in csvreader:
+        #print(row)
+        monthCount += 1
+        totalVolume += int(row[1])
+        if int(row[1]) > greatestIncrease:
+            bestMonth = (row[0])
+            greatestIncrease = int(row[1])
+        elif int(row[1]) < greatestDecrease:
+            worstMonth = (row[0])
+            greatestDecrease = int(row[1])
+        change.append(int(row[1]))
+
+  
+# track monthly changes
+for i in range(len(change)-1):
+    monthlyChange = (change[i+1] - change[i])
+    monthToMonthChange.append(monthlyChange)   
+
+averageChange = statistics.mean(monthToMonthChange)
+
+print("Financial Analysis")
+print("___________________________________")
+
+print("Total Months: " + str(monthCount))
+print("Average Change is: $" + str(round(averageChange, 2)))
+print("Total: $" + str(totalVolume))
+print("Greatest Increase in Profits: " + str(bestMonth) + "  ($" + str(greatestIncrease) + ")")
+print("Greatest Decrease in Profits: " + str(worstMonth) + "  ($" + str(greatestDecrease) + ")")
+
+f = open("financial_analysis.txt", "w")
+f.write("Financial Analysis")
+f.write("___________________________________")
+
+f.write("Total Months: " + str(monthCount))
+f.write("Average Change is: $" + str(round(averageChange, 2)))
+f.write("Total: $" + str(totalVolume))
+f.write("Greatest Increase in Profits: " + str(bestMonth) + "  ($" + str(greatestIncrease) + ")")
+f.write("Greatest Decrease in Profits: " + str(worstMonth) + "  ($" + str(greatestDecrease) + ")")
+ 
